c4afe639fb8fc8594c79efdd2fbba66d57dbb808
guven altunsoy <gven.altunsoy@gmail.com>
2020-01-16 00:13:15 +0300

    merge master

---
+++ build/precache-manifest.17e5c20089792f48b3127a3f31768fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb792b88150c5e12b010db2c7b64412",
+    "url": "/algolia-search-usage/index.html"
+  },
+  {
+    "revision": "f3dfdd74edac9aca0949",
+    "url": "/algolia-search-usage/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e61065e149db786082ba",
+    "url": "/algolia-search-usage/static/js/2.abddc83b.chunk.js"
+  },
+  {
+    "revision": "b360b6ca8c8b155f3736660337928c0d",
+    "url": "/algolia-search-usage/static/js/2.abddc83b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3dfdd74edac9aca0949",
+    "url": "/algolia-search-usage/static/js/main.8c0aff9c.chunk.js"
+  },
+  {
+    "revision": "1d3f61b91a575c35d77d",
+    "url": "/algolia-search-usage/static/js/runtime-main.15fc7bd5.js"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/algolia-search-usage/precache-manifest.17e5c20089792f48b3127a3f31768fe9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/algolia-search-usage/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
