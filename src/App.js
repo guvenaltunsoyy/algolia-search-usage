b70e3a71aae0ed672536aeac223f7157b119f19f
guven altunsoy <guvenaltunsoy@guven-MacBook-Pro.local>
2020-01-15 23:47:28 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const algoliasearch = require('algoliasearch');
+const client = algoliasearch('YR9EHTUZLF', '0fa905cc79c37865c895cbd920b33420');
+const index = client.initIndex('test_firstSearchIndex');
+
 function App() {
+  const [searchKey, setSearchKey] = useState("");
+  const [show, setShow] = useState(false);
+  const [results, setResults] = useState();
+
+  function search() {
+    if (searchKey === "") { return; }
+    index.search({ 'query': searchKey })
+      .then(({ hits } = {}) => {
+        setResults(hits);
+      }).catch(err => {
+        console.log(err);
+        console.log(err.debugData);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Algolia Search with firstname
+      </p>
+      <input type="text"
+      placeholder="Search"
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') {
+            search(event);
+            setShow(true);
+          } else {
+            setShow(false);
+          }
+        }}
+        onChange={(event) => setSearchKey(event.target.value)} />
+      {show && results !== undefined &&
+        results.map((value, key) => {
+          return <p key={key}>Firstname : {value.firstname} Lastname : {value.lastname}</p>
+        })
+      }
     </div>
   );
 }
